9869ca3c1282b3891eda0ab1a8aa0ab78a542bbd
lancegabriel <lancegabrielwork@hotmail.sg>
2022-05-15 20:44:45 +0800

    Home page, login and logout function, Moralis authentication integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import { useMoralis} from "react-moralis";
+import { Button, ConnectButton, Typography} from "web3uikit";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 function App() {
+    const { isAuthenticated, Moralis, logout} = useMoralis();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    
+    {isAuthenticated ? (
+    <div className="page">
+      <div className="sideBar"> Sidebar</div>
+          <div className="mainWindow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+            <Button 
+            text="Sign out"
+            onClick={() => logout()}
+            />
+              <div className="Rightbar"> RightBar</div>
+        </div>
+       
+    ) : (
+      <div className="loginPage">
+        <Typography variant="h1" color="white">Post It</Typography>
+        <Typography variant="caption14" color="white">Anonymous and fun.</Typography>
+        <ConnectButton />
+      </div>
+    )}
+    </>
   );
-}
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { NotificationProvider} from "web3uikit";
+import { MoralisProvider } from "react-moralis";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
+    <MoralisProvider appId="7KlZHvrKvJeNHuJRAt1PVWkexo8OcvjTGwvGhYXl" serverUrl="https://93ffnnayutoa.usemoralis.com:2053/server">
+   <NotificationProvider>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </NotificationProvider>
+  </MoralisProvider>,
   </React.StrictMode>
 );
 
